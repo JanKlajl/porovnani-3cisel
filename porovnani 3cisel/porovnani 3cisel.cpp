fe588041994233c8a331052c530822143a698049
majitel <majitel@GLEN>
2021-10-31 16:27:12 +0100

    Přidejte soubory projektu.

---
+++ porovnani 3cisel/porovnani 3cisel.cpp
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+
+int main(void) {
+	int x = 11;
+	int y = 90;
+	int z = 600;
+	// ktere cislo je nejvetsi?
+	if (x > y && x > z) {
+		printf("cislo %d ", x);
+		printf("je vetsi nez cislo %d", y);
+		printf(" i nez cislo %d\n", z);
+	}
+	if (y > x && y > z) {	
+		printf("cislo %d ", y);
+		printf("je vetsi nez cislo %d", x);
+		printf(" i nez cislo %d\n", z);
+	}
+	else if (z > x && z > y) {	 
+		 printf("cislo %d ", z);
+		 printf("je vetsi nez cislo %d", x);
+		 printf(" i nez cislo %d\n", y);
+	}
+} 
